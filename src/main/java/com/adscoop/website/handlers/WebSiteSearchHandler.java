7bde89f052aa3476f767e06446df8ecea70d990b
Tim Gundmann <tim@gundmann.dk>
2017-05-16 18:40:52 +0200

    default values removed

--- src/main/java/com/adscoop/website/handlers/WebSiteSearchHandler.java
+++ src/main/java/com/adscoop/website/handlers/WebSiteSearchHandler.java
@@ -24,28 +24,20 @@ public class WebSiteSearchHandler extends  AbstractTokenHandler{
 
     @Override
     protected void handleWithToken(Context ctx, String token) {
-       String zip =  ctx.getPathTokens().getOrDefault("zip","2000");
-       String country= ctx.getPathTokens().getOrDefault("country","dk");
-       String region = ctx.getPathTokens().getOrDefault("region","copenhagen");
-       String type = ctx.getPathTokens().getOrDefault("type", "* ");
-       String category = ctx.getPathTokens().getOrDefault("category", " *");
-       String visitors = ctx.getPathTokens().getOrDefault("vistors", " *");
-       String gender = ctx.getPathTokens().getOrDefault("gender", "*");
-       int mixAge = Integer.valueOf(ctx.getPathTokens().getOrDefault("minAge", "0"));
-       int maxAge = Integer.valueOf(ctx.getPathTokens().getOrDefault("maxAge", String.valueOf(100)));
-       boolean physicalShop = Boolean.valueOf(ctx.getPathTokens().getOrDefault("physicalShop","false"));
-
        RxRatpack.observe(searchService.
                searchByArea(SearchParams.builder().
-                       category(category).
-                       zip(zip).country(country).
-                       region(region).
-                       type(type).
-                       visitors(visitors).
-                       gender(gender).
-                       mixAge(mixAge).
-                       maxAge(maxAge).
-                       physicalShop(physicalShop).build())).forEach( webSites -> ctx.render(json(webSites))  );
+                       category(ctx.getPathTokens().getOrDefault("category", " *")).
+                       zip(ctx.getPathTokens().getOrDefault("zip","")).
+                       country(ctx.getPathTokens().getOrDefault("country","")).
+                       region(ctx.getPathTokens().getOrDefault("region","")).
+                       type(ctx.getPathTokens().getOrDefault("type", "* ")).
+                       visitors(ctx.getPathTokens().getOrDefault("vistors", " *")).
+                       gender(ctx.getPathTokens().getOrDefault("gender", "*")).
+                       mixAge(Integer.valueOf(ctx.getPathTokens().getOrDefault("minAge", ""))).
+                       maxAge(Integer.valueOf(ctx.getPathTokens().getOrDefault("maxAge", ""))).
+                       physicalShop(Boolean.valueOf(ctx.getPathTokens().getOrDefault("physicalShop",""))).
+                       build()))
+       			.forEach( webSites -> ctx.render(json(webSites))  );
 
     }
 }
--- src/test/java/com/adscoop/website/handlers/SearchServiceTestHandler.java
+++ src/test/java/com/adscoop/website/handlers/SearchServiceTestHandler.java
@@ -1,66 +1,36 @@
 package com.adscoop.website.handlers;
 
-import com.adscoop.website.StartWebsiteServcie;
-import com.adscoop.website.entites.SearchParams;
-import com.adscoop.website.entites.WebSite;
-import com.adscoop.website.services.SearchService;
-import com.adscoop.website.utils.RxRule;
-import org.junit.Before;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import ratpack.exec.Promise;
+
+import com.adscoop.website.StartWebsiteServcie;
+import com.adscoop.website.utils.RxRule;
+
 import ratpack.http.Status;
 import ratpack.test.MainClassApplicationUnderTest;
-import ratpack.test.handling.RequestFixture;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.when;
-
-/**
- * Created by thokle on 11/05/2017.
- */
-
-@RunWith(MockitoJUnitRunner.class )
+@RunWith(MockitoJUnitRunner.class)
 public class SearchServiceTestHandler {
 
-    private static final String USER_TOKEN = "user_token";
+	private static final String USER_TOKEN = "user_token";
 
-    @Rule
-    public RxRule rxRule = new RxRule();
+	@Rule
+	public RxRule rxRule = new RxRule();
 
-    @Mock
-    private SearchService searchService;
+	@Test
+	public void testPrefixUforSearch() {
+		try (MainClassApplicationUnderTest service = new MainClassApplicationUnderTest(StartWebsiteServcie.class)) {
+			assertThat("Http request search was ok",
+					service.getHttpClient().requestSpec(r -> r.getHeaders().add(Const.Headers.TOKEN, USER_TOKEN))
+							.get("search/:zip?/:country?/:region?/:category?/:gender?/:minAge?/:maxAge?/:type?/:visitors?/:physicalShop")
+							.getStatus(),
+					equalTo(Status.OK));
+		}
+	}
 
-    private  WebSiteSearchHandler webSiteSearchHandler;
-
-    @Before
-    public void setup(){
-        webSiteSearchHandler  = new WebSiteSearchHandler(searchService);
-
-    }
-
-    @Test
-    public void testPrefixUforSearch(){
-        try (MainClassApplicationUnderTest service = new MainClassApplicationUnderTest(StartWebsiteServcie.class)) {
-
-            assertThat("Http request status was  ok",
-                    service.getHttpClient().requestSpec(r -> r.getHeaders().add(Const.Headers.TOKEN, USER_TOKEN))
-                            .get("search/:zip?/:country?/:region?/:category?/:gender?/:minAge?/:maxAge?/:type?/:visitors?/:physicalShop" ).getStatus(),
-                    equalTo(Status.OK));
-
-        }
-    }
-
-
-
-
-    }
-
+}
